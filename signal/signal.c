59c027993fc84a92cae3dbc79fd99c412403b7ab
uggurr <uggurryldz@gmail.com>
2023-08-22 11:36:33 +0300

    finish

--- signal/signal.c
+++ signal/signal.c
@@ -6,7 +6,7 @@
 /*   By: uguyildi <uguyildi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 13:29:41 by uguyildi          #+#    #+#             */
-/*   Updated: 2023/08/22 11:31:03 by uguyildi         ###   ########.fr       */
+/*   Updated: 2023/08/22 11:35:56 by uguyildi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,8 @@ static void	handle_sigint(int sig)
 {
 	(void)sig;
 	write(1, "\n", 1);
-	rl_replace_line("", 0);
 	rl_on_new_line();
+	rl_replace_line("", 0);
 	rl_redisplay();
 	g_glbl.erorno = 1;
 }
